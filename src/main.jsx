979e33339f17cecc98fc43cb66b5a43f2f56bdd7
israt6898 <israt6898@gmail.com>
2024-06-02 20:11:10 +0600

    finish singup design

--- src/main.jsx
+++ src/main.jsx
@@ -10,10 +10,27 @@ import {
 import { router } from './Routes/Routes';
 import { HelmetProvider } from 'react-helmet-async';
 
+import {
+  QueryClient,
+  QueryClientProvider,
+  
+} from '@tanstack/react-query'
+import AuthProvider from './Provider/AuthProvider';
+import { ToastContainer } from 'react-toastify';
+
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <HelmetProvider>
+
+  <QueryClientProvider client={queryClient}>
+  <AuthProvider>
   <RouterProvider router={router} />
+  <ToastContainer />
+  </AuthProvider>
+    </QueryClientProvider>
+
   </HelmetProvider>
   </React.StrictMode>,
 )
